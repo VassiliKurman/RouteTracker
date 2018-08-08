1a1266bb4c1d98d951ad20f57f169bd6bfcead32
Instructor79 <instructor79vk@googlemail.com>
2018-08-08 15:24:36 +0100

    feature: added helper methods to TrackerDbUtils.java
    - added deleting and updating objects in database helper methods to TrackerDbUtils.java class

--- app/src/main/java/vkurman/routetracker/provider/TrackerDbUtils.java
+++ app/src/main/java/vkurman/routetracker/provider/TrackerDbUtils.java
@@ -24,7 +24,9 @@ import vkurman.routetracker.model.User;
 import vkurman.routetracker.model.Waypoint;
 
 /**
- * TrackerDbUtils
+ * TrackerDbUtils is a helper class to simplify CRUD operations: creating,retrieving,
+ * updating and deleting operations in database.
+ *
  * Created by Vassili Kurman on 07/08/2018.
  * Version 1.0
  */
@@ -34,26 +36,29 @@ public class TrackerDbUtils {
      *
      * @param context
      * @param user
+     * @return Uri - uri pointing to row inserted
      */
     public static Uri addUser(Context context, User user) {
         // Create new empty ContentValues object
         ContentValues userContentValues = new ContentValues();
-        // Put the task description and selected mPriority into the ContentValues
+        // Put data into the ContentValues
         userContentValues.put(TrackerContract.UsersEntry.COLUMN_USERS_ID, user.getId());
         userContentValues.put(TrackerContract.UsersEntry.COLUMN_USERS_NAME, user.getName());
         // Insert the content values via a ContentResolver
         return context.getContentResolver().insert(TrackerContract.UsersEntry.CONTENT_URI_USERS, userContentValues);
     }
+
     /**
      * Saves track into database. No waypoints are saved in here.
      *
      * @param context
      * @param track
+     * @return Uri - uri pointing to row inserted
      */
     public static Uri addTrack(Context context, Track track) {
         // Create new empty ContentValues object
         ContentValues trackContentValues = new ContentValues();
-        // Put the task description and selected mPriority into the ContentValues
+        // Put data into the ContentValues
         trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_ID, track.getId());
         trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_NAME, track.getName());
         trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_OWNER, track.getOwner());
@@ -61,16 +66,18 @@ public class TrackerDbUtils {
         // Insert the content values via a ContentResolver
         return context.getContentResolver().insert(TrackerContract.TracksEntry.CONTENT_URI_TRACKS, trackContentValues);
     }
+
     /**
      * Saves waypoint into database
      *
      * @param context
      * @param waypoint
+     * @return Uri - uri pointing to row inserted
      */
     public static Uri addWaypoint(Context context, Waypoint waypoint) {
         // Create new empty ContentValues object
         ContentValues waypointContentValues = new ContentValues();
-        // Put the task description and selected mPriority into the ContentValues
+        // Put data into the ContentValues
         waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_ID, waypoint.getId());
         waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_TRACK_ID, waypoint.getTrackId());
         waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_TIMESTAMP, waypoint.getTimeStamp());
@@ -78,6 +85,110 @@ public class TrackerDbUtils {
         waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_LONGITUDE, waypoint.getLongitude());
         waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_ALTITUDE, waypoint.getAltitude());
         // Insert the content values via a ContentResolver
-        return context.getContentResolver().insert(TrackerContract.UsersEntry.CONTENT_URI_USERS, waypointContentValues);
+        return context.getContentResolver().insert(TrackerContract.WaypointsEntry.CONTENT_URI_WAYPOINTS, waypointContentValues);
+    }
+
+    /**
+     * Removes user with specified id from database
+     *
+     * @param context
+     * @param userId
+     * @return int - number of rows deleted
+     */
+    public static int removeUser(Context context, String userId) {
+        String selection = TrackerContract.UsersEntry.COLUMN_USERS_ID + "=?";
+        String[] selectionArgs = new String[]{userId};
+        // Delete the content values via a ContentResolver
+        return context.getContentResolver().delete(TrackerContract.UsersEntry.CONTENT_URI_USERS, selection, selectionArgs);
+    }
+
+    /**
+     * Removes track with specified id from database. No waypoints are removed in here.
+     *
+     * @param context
+     * @param trackId
+     * @return int - number of rows deleted
+     */
+    public static int removeTrack(Context context, long trackId) {
+        String selection = TrackerContract.TracksEntry.COLUMN_TRACKS_ID + "=?";
+        String[] selectionArgs = new String[]{Long.toString(trackId)};
+        // Delete the content values via a ContentResolver
+        return context.getContentResolver().delete(TrackerContract.TracksEntry.CONTENT_URI_TRACKS, selection, selectionArgs);
+    }
+
+    /**
+     * Removes all waypoints from database for specified track id.
+     *
+     * @param context
+     * @param trackId
+     * @return int - number of rows deleted
+     */
+    public static int removeWaypoints(Context context, long trackId) {
+        String selection = TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_TRACK_ID + "=?";
+        String[] selectionArgs = new String[]{Long.toString(trackId)};
+        // Delete the content values via a ContentResolver
+        return context.getContentResolver().delete(TrackerContract.WaypointsEntry.CONTENT_URI_WAYPOINTS, selection, selectionArgs);
+    }
+
+    /**
+     * Updates user in database
+     *
+     * @param context
+     * @param user
+     * @return int - number of rows updated
+     */
+    public static int updateUser(Context context, User user) {
+        // Create new empty ContentValues object
+        ContentValues userContentValues = new ContentValues();
+        // Put data into the ContentValues
+        userContentValues.put(TrackerContract.UsersEntry.COLUMN_USERS_NAME, user.getName());
+
+        String selection = TrackerContract.UsersEntry.COLUMN_USERS_ID + "=?";
+        String[] selectionArgs = new String[]{user.getId()};
+        // Insert the content values via a ContentResolver
+        return context.getContentResolver().update(TrackerContract.UsersEntry.CONTENT_URI_USERS, userContentValues, selection, selectionArgs);
+    }
+
+    /**
+     * Updates track in database. No waypoints are updated in here.
+     *
+     * @param context
+     * @param track
+     * @return int - number of rows updated
+     */
+    public static int updateTrack(Context context, Track track) {
+        // Create new empty ContentValues object
+        ContentValues trackContentValues = new ContentValues();
+        // Put data into the ContentValues
+        trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_NAME, track.getName());
+        trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_OWNER, track.getOwner());
+        trackContentValues.put(TrackerContract.TracksEntry.COLUMN_TRACKS_IMAGE, track.getImage());
+
+        String selection = TrackerContract.TracksEntry.COLUMN_TRACKS_ID + "=?";
+        String[] selectionArgs = new String[]{Long.toString(track.getId())};
+        // Insert the content values via a ContentResolver
+        return context.getContentResolver().update(TrackerContract.TracksEntry.CONTENT_URI_TRACKS, trackContentValues, selection, selectionArgs);
+    }
+
+    /**
+     * Updates waypoint in database
+     *
+     * @param context
+     * @param waypoint
+     * @return int - number of rows updated
+     */
+    public static int updateWaypoint(Context context, Waypoint waypoint) {
+        // Create new empty ContentValues object
+        ContentValues waypointContentValues = new ContentValues();
+        // Put data into the ContentValues
+        waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_TIMESTAMP, waypoint.getTimeStamp());
+        waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_LATITUDE, waypoint.getLatitude());
+        waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_LONGITUDE, waypoint.getLongitude());
+        waypointContentValues.put(TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_ALTITUDE, waypoint.getAltitude());
+
+        String selection = TrackerContract.WaypointsEntry.COLUMN_WAYPOINTS_ID + "=?";
+        String[] selectionArgs = new String[]{Long.toString(waypoint.getId())};
+        // Insert the content values via a ContentResolver
+        return context.getContentResolver().update(TrackerContract.WaypointsEntry.CONTENT_URI_WAYPOINTS, waypointContentValues, selection, selectionArgs);
     }
 }
