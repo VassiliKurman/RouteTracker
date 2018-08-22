42f48f17c726be8d7a8d3ff1ec3977ccf1a00ee4
Vassili Kurman <instructor79vk@googlemail.com>
2018-08-22 11:11:13 +0100

    refactor: minor adjustments done to SharedTracksAdapter.java to display user name
    - changed SharedTracksAdapter.java to display user name if it is not empty

--- app/src/main/java/vkurman/routetracker/adapter/SharedTracksAdapter.java
+++ app/src/main/java/vkurman/routetracker/adapter/SharedTracksAdapter.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -148,7 +149,7 @@ public class SharedTracksAdapter extends RecyclerView.Adapter<SharedTracksAdapte
         if (position >= 0 && position < mTracks.size()) {
             for (Track track : mTracks) {
                 String name = track.getName();
-                String owner = track.getOwner();
+                String owner = TextUtils.isEmpty(track.getOwnerName()) ? track.getOwner() : track.getOwnerName();
                 String image = track.getImage();
                 long timestamp = track.getId();
 
